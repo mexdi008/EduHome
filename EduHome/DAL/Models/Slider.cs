d3dcd96049c409f4e2e23aaed802f2b581440b52
Mehti Sadixov <bbb-eee-800@mail.ru>
2022-06-08 00:23:41 +0400

    eduhomefirstpush
    eduhomefirstpush

---
+++ EduHome/DAL/Models/Slider.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Base;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAL.Models
+{
+    internal class Slider : BaseEntity, IEntity
+    {
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [MaxLength(255)]
+        public string Body { get; set; }
+        public string ImgUrl { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+    }
+}
